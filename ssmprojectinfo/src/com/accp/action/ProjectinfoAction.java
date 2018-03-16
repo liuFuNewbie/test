b74ff822db8b8032673647ad2627c23674b21180
liuFuNewbie <1191642476@qq.com>
2018-03-16 09:50:17 +0800

    v2

--- ssmprojectinfo/src/com/accp/action/ProjectinfoAction.java
+++ ssmprojectinfo/src/com/accp/action/ProjectinfoAction.java
@@ -24,22 +24,12 @@ public class ProjectinfoAction {
 	@RequestMapping("/getProjectinfoByStatus")
 	public String getProjectinfoByStatus(
 			Model model,
-			@RequestParam(value = "status", required = false) String status,
-			@RequestParam(value = "currentPageNo", required = false) String currentPageNo) {
+			@RequestParam(value = "status", required = false) Integer status,
+			@RequestParam(value = "currentPageNo", required = false,defaultValue="1") Integer currentPageNo) {
 		int pageSize = 3;
 		int totalCount = 0;
-		int s = -1;
-		int curr = 1;
-		System.out.println(status);
-		System.out.println(currentPageNo);
-		if (status != null && status != "") {
-			s = Integer.valueOf(status);
-		}
-		if (currentPageNo != null && currentPageNo != "") {
-			curr = Integer.valueOf(currentPageNo);
-		}
 		try {
-			totalCount = projectinfoService.getProjectinfoCount(s);
+			totalCount = projectinfoService.getProjectinfoCount(status);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -50,11 +40,11 @@ public class ProjectinfoAction {
 		// 设置总行数
 		pageSupport.setTotalCount(totalCount);
 		// 设置当前页数
-		pageSupport.setCurrentPageNo(curr);
+		pageSupport.setCurrentPageNo(currentPageNo);
 		// 创建对象
 		List<Projectinfo> projectinfoList = null;
 		try {
-			projectinfoList = projectinfoService.getProjectinfoByStatus(s,
+			projectinfoList = projectinfoService.getProjectinfoByStatus(status,
 					pageSupport);
 		} catch (Exception e) {
 			e.printStackTrace();
